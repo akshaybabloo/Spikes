66854877f5b25c5e175a90f38b5f2422864e4c84
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-02-16 17:31:51 +1300

    cross validation depricated

--- Spikes/Utility.py
+++ Spikes/Utility.py
@@ -10,7 +10,7 @@ import os
 import re
 
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 
 __all__ = ['ReadCSV', 'ReadJSON']
 
