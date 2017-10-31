e1930ca12f6ab24b46b539280d6fd8126ef85ade
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-31 14:22:37 +1300

    tests updated

--- tests/run.py
+++ tests/run.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 import numpy.testing as npt
 
-from Spikes.Utility import ReadCSV
-from Spikes.Encoder import BSA, TBR
+from Spikes.utility import ReadCSV
+from Spikes.encoder import BSA, TBR
 
 data_location = os.path.abspath('Data')
 
