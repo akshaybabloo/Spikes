195107ea21ad757f196df60b8fea8e9b5a30354a
akshay raj gollahalli <akshaybabloo@gmail.com>
2019-03-31 15:36:58 +1300

    BSA tests added

---
+++ tests/test_bsa.py
@@ -0,0 +1,23 @@
+import numpy as np
+import numpy.testing as npt
+
+from spikes.encoder import BSA
+from spikes.utility import ReadCSV
+
+
+def test_get_spikes():
+    test_spikes = np.load('test_spikes_BSA.npy')
+    data = ReadCSV('../Data').get_samples()['samples']
+
+    spikes = BSA(data).get_spikes()
+
+    npt.assert_array_equal(spikes, test_spikes)
+
+
+def test_get_spikes_time():
+    test_spikes_time = np.load('test_spikes_time_BSA.npy')
+    data = ReadCSV('../Data').get_samples()['samples']
+
+    spikes_time = BSA(data).get_spike_time()
+
+    npt.assert_array_equal(spikes_time, test_spikes_time)
