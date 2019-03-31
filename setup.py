f77f7bf3d13bdae092abf7497bb5a8d2695754ae
akshay raj gollahalli <akshaybabloo@gmail.com>
2019-03-31 14:09:24 +1300

    Python 2.7, 3.2, 3.3, and 3.4 dropped and python 3.7 support added

--- setup.py
+++ setup.py
@@ -41,13 +41,10 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "License :: OSI Approved"
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
     ]
 )
