33439532e49e84fe0c37efb8850a67e67fcae71e
akshay raj gollahalli <akshaybabloo@gmail.com>
2019-03-31 14:18:45 +1300

    base added for encoder

---
+++ spikes/base.py
@@ -0,0 +1,20 @@
+"""
+Base file for all classes.
+"""
+
+
+class Encoder:
+    """
+    Base for encoders.
+    """
+    def __init__(self):
+        pass
+
+    def get_spikes(self):
+        pass
+
+    def get_spike_time(self):
+        pass
+
+    def save(self):
+        pass
