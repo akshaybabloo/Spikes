d09960f592fb63bcda8adcf8bcaf7d405433b2ad
Akshay <akshaybabloo@gmail.com>
2020-09-06 19:01:33 +1200

    paths corrected and tests files recreated with dtype object

--- tests/test_bsa.py
+++ tests/test_bsa.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import numpy.testing as npt
 
@@ -6,8 +8,8 @@ from spikes.utility import ReadCSV
 
 
 def test_get_spikes():
-    test_spikes = np.load('./test_spikes_BSA.npy')
-    data = ReadCSV('../Data').get_samples()['samples']
+    test_spikes = np.load(os.path.join('tests', 'test_spikes_BSA.npy'))
+    data = ReadCSV('./Data').get_samples()['samples']
 
     spikes = BSA(data).get_spikes()
 
@@ -15,8 +17,8 @@ def test_get_spikes():
 
 
 def test_get_spikes_time():
-    test_spikes_time = np.load('./test_spikes_time_BSA.npy')
-    data = ReadCSV('../Data').get_samples()['samples']
+    test_spikes_time = np.load(os.path.join('tests', 'test_spikes_time_BSA-1.npy'), allow_pickle=True)
+    data = ReadCSV('./Data').get_samples()['samples']
 
     spikes_time = BSA(data).get_spike_time()
 
